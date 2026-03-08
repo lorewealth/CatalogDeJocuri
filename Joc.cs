063194da68b50025bdf7c43df0b4761b54cb8b17
lorewealth <lorewealth@gmail.com>
2026-03-08 23:45:21 +0200

    Am descris putin functionarea jocului. Am creat o clasa numita "Joc", care va contine detalii despre un joc

---
+++ Joc.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProiectCatalogDeJocuri
+{
+    public class Joc
+    {
+        //variabile
+        public int Id { get; private set; }
+        public string Denumirea { get; private set; }
+        public double Pret { get; private set; }
+        public double Rate { get; private set; }
+        public List<string> Genre = new List<string>();
+        public List<string> Platforme = new List<string>();
+        public List<string> Publicatori = new List<string>();
+        public List<string> Dezvoltatori = new List<string>();
+
+        //constructor
+        public Joc(int Id, string Denumirea, double Pret, List<string> Genre, List<string> Platforme, List<string> Publicatori, List<string> Dezvoltatori, double Rate)
+        {
+            this.Id = Id;
+            this.Denumirea = Denumirea;
+            this.Pret = Pret;
+            this.Genre = Genre;
+            this.Platforme = Platforme;
+            if (Rate >= 0.0 && Rate <= 10.0) 
+            {
+                this.Rate = Rate;
+            }
+            else
+            {
+                Console.WriteLine("Rate este setat incorect!");
+                Rate = 0.0;
+            }
+            this.Publicatori = Publicatori;
+            this.Dezvoltatori = Dezvoltatori;
+
+        }
+
+        public void getGenre()
+        {
+            if (Genre.Count > 0)
+            {
+                foreach (var el in Genre)
+                {
+                    Console.WriteLine($"    {el}");
+                }
+            }
+        }
+
+        public void getPlatforme()
+        {
+            if (Platforme.Count > 0)
+            {
+                foreach (var el in Platforme)
+                {
+                    Console.WriteLine($"    {el}");
+                }
+            }
+        }
+
+        public void getPublicatori()
+        {
+            if(Publicatori.Count > 0)
+            {
+                foreach(var el in Publicatori)
+                {
+                    Console.WriteLine($"    {el}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nu a fost initializat publicatorii!");
+            }
+        }
+
+        public void getDezvoltatori()
+        {
+            if(Dezvoltatori.Count > 0)
+            {
+                foreach(var el in Dezvoltatori)
+                {
+                    Console.WriteLine($"    {el}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nu a fost initializat dezvolatorii!");
+            }
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine($"Detalii jocului: {Denumirea}");
+            Console.WriteLine($"Pretul: {Pret}");
+            Console.WriteLine($"Rating: {Rate}/10");
+            Console.WriteLine("Genre: ");
+            getGenre();
+            Console.WriteLine("Platforme: ");
+            getPlatforme();
+            Console.WriteLine("Publicatori: ");
+            getPublicatori();
+            Console.WriteLine("Dezvoltatori: ");
+            getDezvoltatori();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProiectCatalogDeJocuri
 {
@@ -6,6 +7,66 @@ namespace ProiectCatalogDeJocuri
     {
         static void Main(string[] args)
         {
+            int nrDeGenre = 0;
+            int nrDePlatforme = 0;
+            int nrDeDezvoltatori = 0;
+            int nrDePublicatori = 0;
+            string denumirea;
+            int id = 0;
+            double pret = 0.0;
+            double rate = 0.0;
+            List<string> Genre = new List<string>();
+            List<string> Platforme = new List<string>();
+            List<string> Dezvoltatori = new List<string>();
+            List<string> Publicatori = new List<string>();
+
+            Console.Write("Denumirea jocului: ");
+            denumirea = Console.ReadLine();
+
+            Console.Write("Id jocului: ");
+            int.TryParse(Console.ReadLine(), out id);
+
+            Console.Write("Pretul jocului: ");
+            double.TryParse(Console.ReadLine(), out pret);
+
+            Console.Write("Rata(Rating) jocului: ");
+            double.TryParse(Console.ReadLine(), out rate);
+
+            Console.Write("Cate genre vreti sa adaugati: ");
+            int.TryParse(Console.ReadLine(), out nrDeGenre);
+            for (int i = 0; i < nrDeGenre; i++)
+            {
+                Console.Write($"Genrul[{i+1}]: ");
+                Genre.Add(Console.ReadLine());
+            }
+
+            Console.Write("Pe cate platforme joaca este disponibila: ");
+            int.TryParse(Console.ReadLine(), out nrDePlatforme);
+            for(int i = 0; i < nrDePlatforme; i++)
+            {
+                Console.Write($"Platforma[{i+1}]: ");
+                Platforme.Add(Console.ReadLine());
+            }
+
+            Console.Write("Cate dezvoltatori au dezvoltat joaca: ");
+            int.TryParse(Console.ReadLine(), out nrDeDezvoltatori);
+            for(int i = 0; i < nrDeDezvoltatori; i++)
+            {
+                Console.Write($"Dezvoltator[{i+1}]: ");
+                Dezvoltatori.Add(Console.ReadLine());
+            }
+            Console.Write("Cate publicatori au publicat joaca: ");
+            int.TryParse(Console.ReadLine(), out nrDePublicatori);
+            for(int i = 0; i < nrDePublicatori; i++)
+            {
+                Console.Write($"Publicator[{i+1}]: ");
+                Publicatori.Add(Console.ReadLine());
+            }
+
+
+            Joc Test = new Joc(id, denumirea, pret, Genre, Platforme, Publicatori, Dezvoltatori, rate);
+
+            Test.getInfo();
 
         }
     }
